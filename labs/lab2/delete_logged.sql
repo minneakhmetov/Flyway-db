31fa781c1066ec3f5d5e5dd0122a638284d8aa44
saiple <32297210+saiple@users.noreply.github.com>
2020-03-17 00:02:07 +0300

    edit scripts

--- labs/lab2/delete_logged.sql
+++ labs/lab2/delete_logged.sql
@@ -3,7 +3,6 @@ $$
     declare
         rand int := floor(random() * 50000 + 1)::int;
     begin
-        ;
         delete from logged_table where id = rand;
     end;
 $$
--- labs/lab2/update_unlogged.sql
+++ labs/lab2/update_unlogged.sql
@@ -3,7 +3,6 @@ $$
     declare
         rand int := floor(random() * 50000 + 1)::int;
     begin
-        ;
         update unlogged_table set field='cwecw' where id = rand;
     end;
 $$

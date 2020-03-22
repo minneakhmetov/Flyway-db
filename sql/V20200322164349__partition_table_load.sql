dcdaa33a3302c911d2468f441908ee666f59bd51
saiple <32297210+saiple@users.noreply.github.com>
2020-03-22 17:46:46 +0300

    Update V20200322164349__partition_table_load.sql

--- sql/V20200322164349__partition_table_load.sql
+++ sql/V20200322164349__partition_table_load.sql
@@ -2,7 +2,7 @@ DO
 $do$
     BEGIN
         FOR i IN 1..1000000 LOOP
-            insert into partitions.partition_table_5(new_field) values (floor(random() * 50000 + 1)::int);
+            insert into partitions.partition_table_10(new_field) values (floor(random() * 50000 + 1)::int);
         END LOOP;
     END
 $do$;

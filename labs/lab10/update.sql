46a3692fded0bff85fdb0a4675bc3154f8a3796a
Razil Minneahmetov <razil.minneahmetov@mera.com>
2020-04-19 23:58:37 +0300

    add sjns;dv

--- labs/lab10/update.sql
+++ labs/lab10/update.sql
@@ -1,6 +1,6 @@
-\set id random(1,1000000)
-\set id_1 random(1,1000000)
-\set id_2 random(1,1000000)
+\set id random(1,425488)
+\set id_1 random(1,425488)
+\set id_2 random(1,425488)
 begin;
 update triggered_table set id = :id, name = :id_2 where id = :id_1;
 commit;
--- sql/V20200419211512__triggers.sql
+++ sql/V20200419211512__triggers.sql
@@ -11,10 +11,11 @@ CREATE OR REPLACE FUNCTION process_emp_audit() RETURNS TRIGGER AS $emp_audit$
         RETURN OLD;
     ELSIF (TG_OP = 'UPDATE') THEN
         if ceil(new.id / 10000) = ceil(old.id / 10000) then
-            EXECUTE 'update triggered.table_' || ceil(new.id / 10000) || ' set name = new.name where id = new.id';
+            EXECUTE 'update triggered.table_' || ceil(new.id / 10000) || ' set name = ' || new.name || ' where id = ' || new.id || ';';
         else
             EXECUTE 'delete from triggered.table_' || ceil(old.id / 10000) || ' where id = ' || old.id;
-            EXECUTE 'insert into triggered.table_' || ceil(new.id / 10000) || ' (id, name) values (' || new.id ||',' || new.name || ') where id = ' || new.id;
+
+            EXECUTE 'insert into triggered.table_' || ceil(new.id / 10000) || ' (id, name) values (' || new.id ||',' || new.name || ');';
         end if;
         RETURN NEW;
     ELSIF (TG_OP = 'INSERT') THEN
@@ -29,4 +30,3 @@ $emp_audit$ LANGUAGE plpgsql;
 CREATE TRIGGER emp_audit
     AFTER INSERT OR UPDATE OR DELETE ON triggered_table
     FOR EACH ROW EXECUTE PROCEDURE process_emp_audit();
-

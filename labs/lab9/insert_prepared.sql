099dd1cf82f971c088eed93587ecd81133b5d7bf
Razil Minneahmetov <razil.minneahmetov@mera.com>
2020-04-19 19:55:45 +0300

    Update insert_prepared.sql

--- labs/lab9/insert_prepared.sql
+++ labs/lab9/insert_prepared.sql
@@ -1,6 +1,6 @@
 begin;
 prepare my_ps (text) as insert into prepared_table (name)
                         values ($1);
-execute my_ps(1, 'vjnsklvns');
+execute my_ps('vjnsklvns');
 deallocate my_ps;
 commit;

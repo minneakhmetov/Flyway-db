2e27dbf250e956a67b2a4216ddc9e025c4f4ddd7
saiple <32297210+saiple@users.noreply.github.com>
2020-05-01 17:35:34 +0300

    Update joins.sql

--- labs/lab12/joins.sql
+++ labs/lab12/joins.sql
@@ -3,6 +3,6 @@ from join_1
          inner join join_2 on join_2.id = join_1.id
          full outer join join_3 on join_3.id = join_1.id
          left join join_4 on join_4.id = join_1.id
-         right join join_5 on join_5.id = index_table.a
-         join join_6 on join_5.id = index_table.b
-         inner join join_7 on join_7.id = index_table.c;
+         right join join_5 on join_5.id = join_1.id
+         join join_6 on join_5.id = join_1.id
+         inner join join_7 on join_7.id = join_1.id;

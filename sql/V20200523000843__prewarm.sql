b3859ccb924e6e46a2c713ad05886a3ff94d47e9
saiple <32297210+saiple@users.noreply.github.com>
2020-05-23 00:21:12 +0300

    Create V20200523000843__prewarm.sql

---
+++ sql/V20200523000843__prewarm.sql
@@ -0,0 +1,49 @@
+create table table_1 (
+    id bigserial,
+    name text
+);
+create table table_2 (
+     id bigserial,
+     name text
+);
+create table table_3 (
+     id bigserial,
+     name text
+);
+
+DO
+$do$
+    BEGIN
+        FOR i IN 1..1000 LOOP
+                insert into table_1 (name) values (random_string(15));
+                insert into table_2 (name) values (random_string(15));
+                insert into table_3 (name) values (random_string(15));
+            END LOOP;
+    END
+$do$;
+
+DO
+$do$
+    BEGIN
+        FOR i IN 1..10000 LOOP
+                insert into table_1 (name) values (random_string(15));
+                insert into table_2 (name) values (random_string(15));
+                insert into table_3 (name) values (random_string(15));
+            END LOOP;
+    END
+$do$;
+
+DO
+$do$
+    BEGIN
+        FOR i IN 1..100000 LOOP
+                insert into table_1 (name) values (random_string(15));
+                insert into table_2 (name) values (random_string(15));
+                insert into table_3 (name) values (random_string(15));
+            END LOOP;
+    END
+$do$;
+
+CREATE EXTENSION pg_prewarm;
+
+SELECT pg_prewarm('table_1', 'table_2', 'table_3')
